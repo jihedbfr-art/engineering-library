e512d6341e38ef3fa461b3aede740672413d3979
Jihed Ben Arfa <jihedbenarfa2026@gmail.com>
2026-07-16 19:02:19 +0100

    chore(projects): retrait de la vitrine notes-app-microservices (projet abandonne)

--- projects/notes-app-microservices/backend/src/main/java/com/jihedapps/notesapp/dto/NotebookRequest.java
+++
@@ -1,7 +0,0 @@
-package com.jihedapps.notesapp.dto;
-
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
-
-public record NotebookRequest(@NotBlank @Size(max = 100) String name) {
-}
