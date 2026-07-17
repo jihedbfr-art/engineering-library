751da1ee2204bbdefb92f59870d28f0ecd3139fb
Jihed Ben Arfa <jihedbenarfa2026@gmail.com>
2026-07-17 01:00:09 +0100

    nano: add hex-to-rgb

---
+++ projects/nano-projects/hex-to-rgb/README.md
@@ -0,0 +1,9 @@
+# hex-to-rgb
+
+Convertit une couleur hexadécimale (#RRGGBB) en triplet RGB.
+
+Lancer :
+
+```
+python main.py
+```
---
+++ projects/nano-projects/hex-to-rgb/main.py
@@ -0,0 +1,9 @@
+def hex_to_rgb(hex_color: str) -> tuple[int, int, int]:
+    hex_color = hex_color.lstrip("#")
+    return tuple(int(hex_color[i:i + 2], 16) for i in (0, 2, 4))
+
+
+if __name__ == "__main__":
+    h = input("Couleur hex (ex: #FF8800) : ")
+    r, g, b = hex_to_rgb(h)
+    print(f"rgb({r}, {g}, {b})")
