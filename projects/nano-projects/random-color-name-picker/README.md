853d566b4ec421ac636efd4a5f7dd3cfe12b9f8b
Jihed Ben Arfa <jihedbenarfa2026@gmail.com>
2026-07-17 02:04:05 +0100

    nano: add random-color-name-picker

---
+++ projects/nano-projects/random-color-name-picker/README.md
@@ -0,0 +1,8 @@
+# random-color-name-picker
+
+Choisit un nom de couleur au hasard dans une palette fixe.
+
+Lancer :
+```
+python main.py
+```
---
+++ projects/nano-projects/random-color-name-picker/main.py
@@ -0,0 +1,7 @@
+import random
+
+COLORS = ["Corail", "Turquoise", "Indigo", "Ambre", "Magenta", "Olive", "Saumon", "Cyan"]
+
+
+if __name__ == "__main__":
+    print(random.choice(COLORS))
